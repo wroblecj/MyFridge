b82aab07414049f4892b30c2ec1e3b2bd9f0003d
Jasrah85 <alainna.nichols85@gmail.com>
2020-03-25 19:24:16 -0400

    Committing changes from Code Review Suggestions #5.

--- app/src/main/java/com/example/myfridgehome/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/myfridgehome/ui/main/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.myfridgehome.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.myfridgehome.dto.Recipe
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var _recipes: MutableLiveData<ArrayList<Recipe>> = MutableLiveData<ArrayList<Recipe>>()
 }
