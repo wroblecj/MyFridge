a009eeb41779ef0b66dd9ece3ab68a8e6da800ba
Dschneider16 <47878831+Dschneider16@users.noreply.github.com>
2020-04-04 13:35:09 -0600

    Removed unused imports, unless there for future use it is unnecessary space taken

--- app/src/main/java/com/example/myfridgehome/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myfridgehome/ui/main/MainFragment.kt
@@ -3,9 +3,7 @@
 package com.example.myfridgehome.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
